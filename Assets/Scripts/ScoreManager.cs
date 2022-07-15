c1aee652d268e7cd6a41fcc0bee874e6f8d3ac82
Josef Stolz <josef.stolz@outlook.com>
2022-07-15 15:48:04 +0200

    basic shroom countdown
    still missing: reset impact of the shrooms, nicer look&feel

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -7,8 +7,14 @@ public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
     public TMPro.TMP_Text pilzScoreText;
+    public TMPro.TMP_Text pilzTimer;
 
     int pilzScore = 0;
+
+    private bool TimerOn = false;
+    private float TimeLeft;
+
+    private string CurrentShroom = "";
 
     private void Awake() {
         instance = this;
@@ -18,17 +24,51 @@ public class ScoreManager : MonoBehaviour
     void Start()
     {
         pilzScoreText.text = "Score: " + pilzScore.ToString();
+        pilzTimer.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(TimerOn)
+        {
+            if(TimeLeft > 0)
+            {
+                TimeLeft -= Time.deltaTime;
+                UpdateTimer(TimeLeft);
+            } 
+            else
+            {
+                pilzTimer.text = "";
+                TimerOn = false;
+                TimeLeft = 0;
+                RemoveShroomImpact(CurrentShroom);
+            }
+        }
     }
 
     // Add Shroom to counter
-    public void AddShroom() {
+    public void AddShroom()
+    {
         pilzScore += 1;
         pilzScoreText.text = "Score: " + pilzScore.ToString();
+    }
+
+    public void ActivateShroomTimer(float seconds, string shroomType)
+    {
+        TimeLeft = seconds;
+        TimerOn = true;
+        CurrentShroom = shroomType;
+    }
+
+    void UpdateTimer(float currentTime)
+    {
+        float seconds = Mathf.FloorToInt(currentTime);
+        pilzTimer.text = CurrentShroom + ": " + seconds.ToString();
+    }
+
+    public void RemoveShroomImpact(string Shroom)
+    {
+        CurrentShroom = "";
     }
 }
