463271f775f5371b069445b53e465b8247e88174
Lea-Pietsch <107213167+Lea-Pietsch@users.noreply.github.com>
2022-07-23 10:37:47 +0200

    CameraMovement

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
+    [SerializeField] Rigidbody2D playerrigid;
     
     private Func<Vector3> GetCameraFollowPosition;
+    private Vector3 cameraShift;
+    private bool coroutineRunning=false;
 
     public void Setup(Func<Vector3> GetCameraFollowPosition){
         this.GetCameraFollowPosition = GetCameraFollowPosition;
+        this.cameraShift = new Vector3(0,10,0);
     }
 
 
@@ -18,7 +22,20 @@ public class FollowPlayer : MonoBehaviour
     {
         Vector3 cameraFollowPosition = GetCameraFollowPosition();
         cameraFollowPosition.z = transform.position.z;
-        Vector3 cameraShift= new Vector3(0,25,0);
+
+        if(!coroutineRunning)
+        {
+            StartCoroutine(shift());
+        }
+        
         transform.position = cameraFollowPosition+cameraShift;
+    }
+
+    IEnumerator shift()
+    {
+        coroutineRunning = true;
+        yield return new WaitForSeconds(1);
+        this.cameraShift= new Vector3(0,playerrigid.gravityScale*2,0);  
+        coroutineRunning = false;
     }
 }
